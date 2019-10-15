ca4ceb4ab3f31333ba5bba7a00e2479215100db8
Beckmann <henriquebeckmann10@gmail.com>
2019-10-15 09:13:02 -0300

    get/set da posicao da peça

--- src/modelo/Peca.java
+++ src/modelo/Peca.java
@@ -5,5 +5,13 @@ public class Peca {
 	private Cor cor;
 	private Posicao posicao;
 	private boolean nuncaMovida;
-
+	
+	public Posicao getPosicao() {
+		return posicao;
+	}
+	public void setPosicao(Posicao posicao) {
+		this.posicao = posicao;
+	}
+	
+	
 }
