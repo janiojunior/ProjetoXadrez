fdb1616e65000b84d393d788b090588f6cabcec1
henri <henri@LAPTOP-1VH8ACQ3>
2019-10-26 08:37:38 -0300

    inicio do tabuleiro e definicao do java default

--- src/br/unitins/xadrez/base/Tabuleiro.java
+++ src/br/unitins/xadrez/base/Tabuleiro.java
@@ -1,5 +1,14 @@
 package br.unitins.xadrez.base;
 
+import br.unitins.xadrez.model.Peca;
+import br.unitins.xadrez.model.Posicao;
+
 public class Tabuleiro {
-	int[][] tabuleiro = new int[8][8]; //Matriz tabuleiro
+	// posicao = linha e coluna
+	private Peca[][] matrizTabuleiro = new Peca[8][8]; //Matriz tabuleiro
+	
+	public Peca getPeca(Posicao posicao) {
+		return matrizTabuleiro[posicao.getLinha()][posicao.getColuna()];
+	}
+	
 }
