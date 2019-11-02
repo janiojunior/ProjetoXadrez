48d1cb8448042120720726640cf6a07174539588
Beckmann <henriquebeckmann10@gmail.com>
2019-11-02 17:10:10 -0300

    torre.fix

--- src/br/unitins/xadrez/model/Torre.java
+++ src/br/unitins/xadrez/model/Torre.java
@@ -8,26 +8,27 @@ public class Torre extends Peca{
 
 	@Override
 	public boolean mover(Posicao posicao) {
-		//Movimento para frente  
-			if(posicao.getLinha()==getPosicao().getLinha()+1||posicao.getLinha()==getPosicao().getLinha()+2|| 
-				posicao.getLinha()==getPosicao().getLinha()+3||posicao.getLinha()==getPosicao().getLinha()+4||
-				posicao.getLinha()==getPosicao().getLinha()+5||posicao.getLinha()==getPosicao().getLinha()+6||
-				posicao.getLinha()==getPosicao().getLinha()+7||
-		//Movimento para trás
-				posicao.getLinha()==getPosicao().getLinha()-1||posicao.getLinha()==getPosicao().getLinha()-2||
-				posicao.getLinha()==getPosicao().getLinha()-3||posicao.getLinha()==getPosicao().getLinha()-4||
-				posicao.getLinha()==getPosicao().getLinha()-5||posicao.getLinha()==getPosicao().getLinha()-6||
-				posicao.getLinha()==getPosicao().getLinha()-7||
-		//Movimento em coluna 
-				posicao.getColuna()==getPosicao().getColuna()+1||posicao.getColuna()==getPosicao().getColuna()+2||
-				posicao.getColuna()==getPosicao().getColuna()+3||posicao.getColuna()==getPosicao().getColuna()+4||
-				posicao.getColuna()==getPosicao().getColuna()+5||posicao.getColuna()==getPosicao().getColuna()+6||
-				posicao.getColuna()==getPosicao().getColuna()+7||
-		//Movimento em coluna voltando
-				posicao.getColuna()==getPosicao().getColuna()-1||posicao.getColuna()==getPosicao().getColuna()-2||
-				posicao.getColuna()==getPosicao().getColuna()-3||posicao.getColuna()==getPosicao().getColuna()-4||
-				posicao.getColuna()==getPosicao().getColuna()-5||posicao.getColuna()==getPosicao().getColuna()-6||
-				posicao.getColuna()==getPosicao().getColuna()-7) {
+//		//Movimento para frente  
+//			if(posicao.getLinha()==getPosicao().getLinha()+1||posicao.getLinha()==getPosicao().getLinha()+2|| 
+//				posicao.getLinha()==getPosicao().getLinha()+3||posicao.getLinha()==getPosicao().getLinha()+4||
+//				posicao.getLinha()==getPosicao().getLinha()+5||posicao.getLinha()==getPosicao().getLinha()+6||
+//				posicao.getLinha()==getPosicao().getLinha()+7||
+//		//Movimento para trás
+//				posicao.getLinha()==getPosicao().getLinha()-1||posicao.getLinha()==getPosicao().getLinha()-2||
+//				posicao.getLinha()==getPosicao().getLinha()-3||posicao.getLinha()==getPosicao().getLinha()-4||
+//				posicao.getLinha()==getPosicao().getLinha()-5||posicao.getLinha()==getPosicao().getLinha()-6||
+//				posicao.getLinha()==getPosicao().getLinha()-7||
+//		//Movimento em coluna 
+//				posicao.getColuna()==getPosicao().getColuna()+1||posicao.getColuna()==getPosicao().getColuna()+2||
+//				posicao.getColuna()==getPosicao().getColuna()+3||posicao.getColuna()==getPosicao().getColuna()+4||
+//				posicao.getColuna()==getPosicao().getColuna()+5||posicao.getColuna()==getPosicao().getColuna()+6||
+//				posicao.getColuna()==getPosicao().getColuna()+7||
+//		//Movimento em coluna voltando
+//				posicao.getColuna()==getPosicao().getColuna()-1||posicao.getColuna()==getPosicao().getColuna()-2||
+//				posicao.getColuna()==getPosicao().getColuna()-3||posicao.getColuna()==getPosicao().getColuna()-4||
+//				posicao.getColuna()==getPosicao().getColuna()-5||posicao.getColuna()==getPosicao().getColuna()-6||
+//				posicao.getColuna()==getPosicao().getColuna()-7) 
+		if(posicao.getLinha()==getPosicao().getLinha()+1 && posicao.getColuna()==getPosicao().getColuna()){
 				return true;
 			}
 		return false;
