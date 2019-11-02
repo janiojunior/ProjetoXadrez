4162eb76863660f458fc62a93bdbf5ae59ee3101
parro <parro@LAPTOP-NKH319NR>
2019-11-02 10:02:01 -0300

    Atualizei a movimentação do peão, mas não está n ponto ainda.

--- src/br/unitins/xadrez/model/Peao.java
+++ src/br/unitins/xadrez/model/Peao.java
@@ -1,16 +1,41 @@
 package br.unitins.xadrez.model;
 public class Peao extends Peca{
-
+	
 	public Peao(int linha, int coluna, Cor cor) {
 		super(linha, coluna, cor);
+		this.setNuncaMovida(true);
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public boolean mover(Posicao posicao) {
-		if(posicao.getLinha()==getPosicao().getLinha()+1) {
-			return true;
+		
+		// Se primeiroMovimento = true ele pode andar uma casa a mais
+		
+		if(this.isNuncaMovida()) {
+			
+			this.setNuncaMovida(false);
+			
+			if((posicao.getColuna() == getPosicao().getColuna()+2 && posicao.getLinha() == getPosicao().getLinha())
+				||(posicao.getColuna() == getPosicao().getColuna()+1 && posicao.getLinha() == getPosicao().getLinha())) {
+				
+					return true;
+			}
+			
 		}
+		
+		// A ideia do if abaixo é verificar se existe uma peca de cor diferente para o peao poder come-la, no entanto, não sei como fazer essa verificacao
+		
+		/*if((posicao.getLinha()+1 && posicao.getColuna()+1 !=)) {
+			
+			if(posicao.getColuna() == getPosicao().getColuna()+1 && posicao.getLinha()==getPosicao().getLinha()+1 
+			   || posicao.getColuna() == getPosicao().getColuna()-1 && posicao.getLinha()==getPosicao().getLinha()+1) {
+				
+				return true;
+			}
+			
+		}*/
+
 		return false;
 	}
 
