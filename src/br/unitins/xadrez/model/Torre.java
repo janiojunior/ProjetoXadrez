b28dd170ef4e2238649607cd11ba682e0601f0fe
Weberth <Valter@LAPTOP-C19FHJOJ>
2019-11-02 16:44:27 -0300

    Adicionei Movimento da Torre.

--- src/br/unitins/xadrez/model/Torre.java
+++ src/br/unitins/xadrez/model/Torre.java
@@ -4,14 +4,32 @@ public class Torre extends Peca{
 
 	public Torre(int linha, int coluna, Cor cor) {
 		super(linha, coluna, cor);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public boolean mover(Posicao posicao) {
-		// TODO Auto-generated method stub
+		//Movimento para frente  
+			if(posicao.getLinha()==getPosicao().getLinha()+1||posicao.getLinha()==getPosicao().getLinha()+2|| 
+				posicao.getLinha()==getPosicao().getLinha()+3||posicao.getLinha()==getPosicao().getLinha()+4||
+				posicao.getLinha()==getPosicao().getLinha()+5||posicao.getLinha()==getPosicao().getLinha()+6||
+				posicao.getLinha()==getPosicao().getLinha()+7||
+		//Movimento para trás
+				posicao.getLinha()==getPosicao().getLinha()-1||posicao.getLinha()==getPosicao().getLinha()-2||
+				posicao.getLinha()==getPosicao().getLinha()-3||posicao.getLinha()==getPosicao().getLinha()-4||
+				posicao.getLinha()==getPosicao().getLinha()-5||posicao.getLinha()==getPosicao().getLinha()-6||
+				posicao.getLinha()==getPosicao().getLinha()-7||
+		//Movimento em coluna 
+				posicao.getColuna()==getPosicao().getColuna()+1||posicao.getColuna()==getPosicao().getColuna()+2||
+				posicao.getColuna()==getPosicao().getColuna()+3||posicao.getColuna()==getPosicao().getColuna()+4||
+				posicao.getColuna()==getPosicao().getColuna()+5||posicao.getColuna()==getPosicao().getColuna()+6||
+				posicao.getColuna()==getPosicao().getColuna()+7||
+		//Movimento em coluna voltando
+				posicao.getColuna()==getPosicao().getColuna()-1||posicao.getColuna()==getPosicao().getColuna()-2||
+				posicao.getColuna()==getPosicao().getColuna()-3||posicao.getColuna()==getPosicao().getColuna()-4||
+				posicao.getColuna()==getPosicao().getColuna()-5||posicao.getColuna()==getPosicao().getColuna()-6||
+				posicao.getColuna()==getPosicao().getColuna()-7) {
+				return true;
+			}
 		return false;
 	}
-
-
 }
