4f37207ce99e483571414a5514c8d5009a0bef93
Eduardo Rocha <eduardo.rocha2999@gmail.com>
2019-11-02 16:48:55 -0300

    Movimento do Bispo

--- src/br/unitins/xadrez/model/Bispo.java
+++ src/br/unitins/xadrez/model/Bispo.java
@@ -6,9 +6,43 @@ public class Bispo extends Peca {
 		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * Formas de movimentação:
+	 * 1. Apenas diagonais, sem limite de casas (+x, +x / +x, -x / -x, -x / -x, +x); (COMPLETO)
+	 */
+	
 	@Override
 	public boolean mover(Posicao posicao) {
-		// TODO Auto-generated method stub
+		if((posicao.getColuna() == getPosicao().getColuna()+1 && posicao.getLinha() == getPosicao().getLinha()+1) 
+				||(posicao.getColuna() == getPosicao().getColuna()+1 && posicao.getLinha() == getPosicao().getLinha()-1) 
+				||(posicao.getColuna() == getPosicao().getColuna()-1 && posicao.getLinha() == getPosicao().getLinha()-1) 
+				||(posicao.getColuna() == getPosicao().getColuna()-1 && posicao.getLinha() == getPosicao().getLinha()+1)
+				||(posicao.getColuna() == getPosicao().getColuna()+2 && posicao.getLinha() == getPosicao().getLinha()+2)
+				||(posicao.getColuna() == getPosicao().getColuna()+2 && posicao.getLinha() == getPosicao().getLinha()-2)
+				||(posicao.getColuna() == getPosicao().getColuna()-2 && posicao.getLinha() == getPosicao().getLinha()-2)
+				||(posicao.getColuna() == getPosicao().getColuna()-2 && posicao.getLinha() == getPosicao().getLinha()+2)
+				||(posicao.getColuna() == getPosicao().getColuna()+3 && posicao.getLinha() == getPosicao().getLinha()+3)
+				||(posicao.getColuna() == getPosicao().getColuna()+3 && posicao.getLinha() == getPosicao().getLinha()-3)
+				||(posicao.getColuna() == getPosicao().getColuna()-3 && posicao.getLinha() == getPosicao().getLinha()-3)
+				||(posicao.getColuna() == getPosicao().getColuna()-3 && posicao.getLinha() == getPosicao().getLinha()+3)
+				||(posicao.getColuna() == getPosicao().getColuna()+4 && posicao.getLinha() == getPosicao().getLinha()+4)
+				||(posicao.getColuna() == getPosicao().getColuna()+4 && posicao.getLinha() == getPosicao().getLinha()-4)
+				||(posicao.getColuna() == getPosicao().getColuna()-4 && posicao.getLinha() == getPosicao().getLinha()-4)
+				||(posicao.getColuna() == getPosicao().getColuna()-4 && posicao.getLinha() == getPosicao().getLinha()+4)
+				||(posicao.getColuna() == getPosicao().getColuna()+5 && posicao.getLinha() == getPosicao().getLinha()+5)
+				||(posicao.getColuna() == getPosicao().getColuna()+5 && posicao.getLinha() == getPosicao().getLinha()-5)
+				||(posicao.getColuna() == getPosicao().getColuna()-5 && posicao.getLinha() == getPosicao().getLinha()-5)
+				||(posicao.getColuna() == getPosicao().getColuna()-5 && posicao.getLinha() == getPosicao().getLinha()+5)
+				||(posicao.getColuna() == getPosicao().getColuna()+6 && posicao.getLinha() == getPosicao().getLinha()+6)
+				||(posicao.getColuna() == getPosicao().getColuna()+6 && posicao.getLinha() == getPosicao().getLinha()-6)
+				||(posicao.getColuna() == getPosicao().getColuna()-6 && posicao.getLinha() == getPosicao().getLinha()-6)
+				||(posicao.getColuna() == getPosicao().getColuna()-6 && posicao.getLinha() == getPosicao().getLinha()+6)
+				||(posicao.getColuna() == getPosicao().getColuna()+7 && posicao.getLinha() == getPosicao().getLinha()+7)
+				||(posicao.getColuna() == getPosicao().getColuna()+7 && posicao.getLinha() == getPosicao().getLinha()-7)
+				||(posicao.getColuna() == getPosicao().getColuna()-7 && posicao.getLinha() == getPosicao().getLinha()-7)
+				||(posicao.getColuna() == getPosicao().getColuna()-7 && posicao.getLinha() == getPosicao().getLinha()+7)) {
+					return true;
+				}
 		return false;
 	}
 
