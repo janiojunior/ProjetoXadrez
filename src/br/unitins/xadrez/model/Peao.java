3da4ab61b370ac2f5da0845dc47c04d6fcd74a4f
Yhan Nunes <Yhan Nunes@192.168.15.8>
2019-11-02 17:01:18 -0300

    Movimento Peao Arrumado

--- src/br/unitins/xadrez/model/Peao.java
+++ src/br/unitins/xadrez/model/Peao.java
@@ -1,19 +1,22 @@
 package br.unitins.xadrez.model;
 public class Peao extends Peca{
-	
+	boolean movInicial = true;
 	public Peao(int linha, int coluna, Cor cor) {
 		super(linha, coluna, cor);
-		this.setNuncaMovida(true);
+//		this.setNuncaMovida(true);
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public boolean mover(Posicao posicao) {
-			if((posicao.getColuna() == getPosicao().getColuna()+2 || posicao.getColuna() == getPosicao().getColuna()-2 )&& posicao.getLinha() == getPosicao().getLinha() && this.isNuncaMovida()
-				||(posicao.getColuna() == getPosicao().getColuna()+1 ||posicao.getColuna() == getPosicao().getColuna()-1 )&& posicao.getLinha() == getPosicao().getLinha()) {
-			this.setNuncaMovida(false);
-					return true;
-			}
+		/*
+		 * if((posicao.getColuna() == getPosicao().getColuna()+2 || posicao.getColuna()
+		 * == getPosicao().getColuna()-2 )&& posicao.getLinha() ==
+		 * getPosicao().getLinha() && this.isNuncaMovida() ||(posicao.getColuna() ==
+		 * getPosicao().getColuna()+1 ||posicao.getColuna() ==
+		 * getPosicao().getColuna()-1 )&& posicao.getLinha() == getPosicao().getLinha())
+		 * { this.setNuncaMovida(false); return true; }
+		 */
 			
 		// A ideia do if abaixo é verificar se existe uma peca de cor diferente para o peao poder come-la, no entanto, não sei como fazer essa verificacao
 		
@@ -26,7 +29,12 @@ public class Peao extends Peca{
 			}
 			
 		}*/
-
+		if(movInicial == true && (posicao.getLinha() == getPosicao().getLinha()+2 || posicao.getLinha() == getPosicao().getLinha()-2 && posicao.getColuna() == getPosicao().getColuna()) || (posicao.getLinha() ==
+				  getPosicao().getLinha()+1 ||posicao.getLinha() ==
+				  getPosicao().getLinha()-1 && posicao.getColuna() == getPosicao().getColuna())) {
+			movInicial = false;
+			return true;
+		}
 		return false;
 	}
 	public boolean capturarPeca(Posicao posicao) {
